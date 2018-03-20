2c3e07c66628d0c4fe94b37991a18cb73243f182
kta <kta@spottykitty.co.uk>
2018-03-20 00:29:42 +0000

    clean unrequired files from repository

--- game_share_debug_pack_HEAVY/top_down_test_game/client/assets/tilemaps/Nimar.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="Nimar" tilewidth="48" tileheight="48" tilecount="3025" columns="55">
- <image source="../images/Nimar.png" width="2640" height="2640"/>
-</tileset>
